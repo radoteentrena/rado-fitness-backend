b82006012af7486dac5f0ec1b8d1ce24c9581aea
Patricio <patriciopherrero@gmail.com>
2026-03-18 12:39:23 -0300

    feat: add payment_failed category to CoachAlert

--- app/models/coach_alert.rb
+++ app/models/coach_alert.rb
@@ -2,7 +2,7 @@ class CoachAlert < ApplicationRecord
   belongs_to :user
 
   enum :status, { pending: 0, resolved: 1, dismissed: 2 }
-  enum :category, { missed_workout: "missed_workout", low_compliance: "low_compliance", weight_spike: "weight_spike", check_in: "check_in", program_complete: "program_complete" }
+  enum :category, { missed_workout: "missed_workout", low_compliance: "low_compliance", weight_spike: "weight_spike", check_in: "check_in", program_complete: "program_complete", payment_failed: "payment_failed" }
 
   validates :category, presence: true
   validates :message, presence: true
